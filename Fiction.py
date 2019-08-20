220b89f7e28e7145bb3413988c74b536b86c2be9
WilliamRui <51812119+WilliamRui@users.noreply.github.com>
2019-08-20 22:41:33 +0800

    Update Fiction.py

--- Fiction.py
+++ Fiction.py
@@ -1,5 +1,11 @@
+####################################################################################################################
 '''
-V01  20190820  Initial Version 
+V01  20190820
+---Initial Version
+V02  20190820
+---在用户输入目标小说ID的时候，加入循环，并在循环中加入判断语句，防止用户输入的ID格式不正确，
+   如用户输入不正确则重新要求用户输入字符
+---在下载小说前后打印正在下载和下载完成字样
 '''
 ####################################################################################################################
 from bs4 import BeautifulSoup
@@ -17,9 +23,9 @@ def GetSoup(url):
 def GetBookInfo():
 #让用户输入小说名称，以一定格式返回搜索结果
 #让用户根据显示的搜索结果选择小说对应的编号，然后函数反馈用户选择小说的地址
-    SearchName = input('请输入小说名：')
+    SearchName = input('请输入搜索关键词：')
     SearchUrl = 'https://www.xbiquge6.com/search.php?keyword=' + SearchName
-    #拼凑搜索地址
+    #拼凑出搜索结果的地址
     Soup = GetSoup(SearchUrl)
     Books = Soup.findAll('div',class_='result-item result-game-item')
     print('{1:{0}<3}\t{2:{0}<8}\t{3:{0}<8}\t{4}'.format(chr(12288),'序号','书名','作者','链接'))
@@ -31,11 +37,13 @@ def GetBookInfo():
         print('{1:{0}<3}\t{2:{0}<8}\t{3:{0}<8}\t{4}'.format(chr(12288),i,BookName,str(Author),BookLink))
         i = i + 1
     BookID = int(input('请输入目标小说ID：'))
-    if BookID>0 and BookID<11:
-    #防止用户输入的小说编号不符合要求
-        return Books[BookID-1].find('a',cpos='title').get('title').strip(),Books[BookID-1].find('a',cpos='title').get('href')
-    else:
-        print('小说序号输入有误，请重新输入！')
+    while True:
+    #防止用户输入的书编号不正确，此处使用死循环加条件语句
+        if BookID>0 and BookID<len(Books)+1:
+        #防止用户输入的小说编号不符合要求
+            return Books[BookID-1].find('a',cpos='title').get('title').strip(),Books[BookID-1].find('a',cpos='title').get('href')
+        else:
+            BookID = int(input('小说序号输入有误，请重新输入:'))
 
 def GetChapterLinks(url):
 #获取小说的章节链接
@@ -49,19 +57,23 @@ def GetChapterLinks(url):
 def GetContent(url):
 #获取小说章节正文
     Soup = GetSoup(url)
-    Content = Soup.find('div',class_='bookname').find('h1').get_text() + '\n' + Soup.find('div',id='content').get_text().replace('\u00A0\u00A0\u00A0\u00A0','\n    ')
+    ChapterName = Soup.find('div',class_='bookname').find('h1').get_text()
+    #获取的正文没有段落换行，但是首行缩进识别了，这里我们将首行缩进替换为换行加四个空格，变相实现段落换行
+    Content = Soup.find('div',id='content').get_text().replace('\u00A0\u00A0\u00A0\u00A0','\n    ')
+    Content = ChapterName + '\n' + Content
     #将取到的章节名和正文结合作为一章
-    #获取的正文没有段落换行，但是首行缩进还是识别了，这里我们将首行缩进替换为换行加四个空格，变相实现段落换行
     return Content
 
 BookName,BookUrl = GetBookInfo()
 ChapterLinks = GetChapterLinks(BookUrl)
 with open(BookName+'.txt','a',encoding='utf-8')as T:
-#以小说名保存小说内容，编码方式为utf-8，此处默认是GBK，编码方式必须要修改
+#以小说名保存小说内容，编码方式此处默认为GBK，修改为utf-8
+    print('正在下载{}。。。'.format(BookName))
     for ChapterLink in ChapterLinks:
     #按章节保存小说
         T.write(GetContent(ChapterLink))
         T.write('\n\n')
         #在两个章节直接预留换行，更加美观。
+    print('下载完成！')
 
     
